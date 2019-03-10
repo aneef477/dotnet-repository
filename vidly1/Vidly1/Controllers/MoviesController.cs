3525f8ce5522fda7758a72906e7636f3c3c81e1c
Aneef Shaik <aneef.cs@gmail.com>
2019-03-10 18:33:26 -0400

    This is my first project

---
+++ vidly1/Vidly1/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using Vidly1.Models;
+
+namespace Vidly1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var Movie = new Movie() { Name = "Raja Hindustani" };
+             return View(Movie);
+
+            //return Content("Hello World !");
+
+            // return HttpNotFound();
+
+            //return new EmptyResult();  
+
+            //return RedirectToAction("Index", "Home", new { page = 1, Sortby ="name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortby)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortby))
+                sortby = "Name";
+            return Content(string.Format("pageIndex = {0}&sortby = {1}", pageIndex, sortby));
+
+
+        }
+
+    }
+}
---
+++ vidly1/Vidly1/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly1.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public String Name { get; set; }
+    }
+}
